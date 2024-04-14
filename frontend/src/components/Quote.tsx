6c5d85eed0370d491d35412dbdf39e56d94c4975
prajwalk97 <prajwalindia18@gmail.com>
2024-04-14 11:46:58 +0530

    init frontend commit

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,7 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-300 h-screen flex flex-col justify-center items-center">
+      Hi from quote component
+    </div>
+  );
+};
---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+import { Quote } from "../components/Quote";
+
+const SignUp = () => {
+  return (
+    <div>
+      <Quote />
+    </div>
+  );
+};
+
+export default SignUp;
